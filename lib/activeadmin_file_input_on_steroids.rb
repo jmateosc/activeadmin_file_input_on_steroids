9fb9a5ed7d98a13cd682a4bc20a0103e9aaba398
Ariel Schvartz <ari.shh@gmail.com>
2014-05-12 19:06:19 -0300

    File path changed

--- lib/activeadmin_file_input_on_steroids.rb
+++ lib/activeadmin_file_input_on_steroids.rb
@@ -1,6 +1,3 @@
 require 'activeadmin_file_input_on_steroids/version'
 require 'activeadmin_file_input_on_steroids/engine'
-require '../app/admin/inputs/file_input'
-
-module ActiveadminFileInputOnSteroids
-end
+require "#{ActiveadminFileInputOnSteroids::Engine.root}/app/admin/inputs/file_input"
